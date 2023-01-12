fbfb2ab88eaf278eb58bdac3d02b4049e9977e89
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-01-12 08:33:19 +0200

    api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,15 @@
 from django.http import JsonResponse
+import json
 
 
 def api_home(request):
-    return JsonResponse({'message': 'django api response'})
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    data['params'] = dict(request.GET)
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,5 +3,5 @@ import requests
 
 endpoint = 'http://127.0.0.1:8000/api/'
 
-get_response = requests.get(endpoint)
+get_response = requests.get(endpoint, params={'abc': 123}, json={'message': 'hello'})
 print(get_response.json())
