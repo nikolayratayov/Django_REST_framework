00b43c71f256862549bc2cbe869c7357d73afaca
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-01-13 16:19:35 +0200

    api

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.db import models
 
 # Create your models here.
@@ -7,3 +9,7 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
+
+    @property
+    def sale_price(self):
+        return f'{(float(self.price) * 1.3):.2f}'
