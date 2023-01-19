e8679e34f57b8ba1e8584e873f3040f36c37d9a2
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-01-19 10:26:59 +0200

    api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import *
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
-    path('', api_home)
+    path('', api_home),
+    path('auth/', obtain_auth_token)
 ]
--- py_client/list.py
+++ py_client/list.py
@@ -1,8 +1,19 @@
 import requests
+from getpass import getpass
 
 
-endpoint = 'http://127.0.0.1:8000/api/products/'
+auth_endpoint = 'http://127.0.0.1:8000/api/auth/'
+username = input('Enter username:\n')
+password = getpass('Enter password:\n')
+auth_response = requests.post(auth_endpoint, json={'username': username, 'password': password})
+print(auth_response.json())
 
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+    endpoint = 'http://127.0.0.1:8000/api/products/'
 
-get_response = requests.get(endpoint)
-print(get_response.json())
+    get_response = requests.get(endpoint, headers=headers)
+    print(get_response.json())
