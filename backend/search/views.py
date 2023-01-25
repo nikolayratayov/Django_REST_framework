ed417a6a223276b829faac048c00dd460544665b
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-01-25 11:59:45 +0200

    api

--- backend/search/views.py
+++ backend/search/views.py
@@ -1,9 +1,27 @@
+import rest_framework.templatetags.rest_framework
 from rest_framework import generics
 from products.models import Product
 from products.serializers import ProductSerializer
+from . import client
+from rest_framework.response import Response
 
 
-class SearchListView(generics.ListAPIView):
+class SearchListView(generics.GenericAPIView):
+    def get(self, *args, **kwargs):
+        user = None
+        if self.request.user.is_authenticated:
+            user= self.request.user.username
+        query = self.request.GET.get('q')
+        public = str(self.request.GET.get('public')) != '0'
+
+        tag = self.request.GET.get('tag') or None
+        if not query:
+            return Response('', status=400)
+        results = client.perform_search(query, tags=tag, user=user, public=public)
+        return Response(results)
+
+
+class SearchListOldView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
