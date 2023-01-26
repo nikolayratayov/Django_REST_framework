13ccc7ca0f69237f27f1714256bb07b6bd31c6ce
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-01-26 10:02:14 +0200

    api

---
+++ js_client/client.js
@@ -0,0 +1,23 @@
+let loginForm = document.getElementById('login-form')
+let baseEndpoint = 'http://localhost:8000/api'
+if (loginForm){
+    loginForm.addEventListener('submit', handleLogin)
+}
+
+function handleLogin(event){
+    event.preventDefault()
+    let loginEndpoint = `${baseEndpoint}/token/`
+    let loginFormData = new FormData(loginForm)
+    let loginObjectData = Object.fromEntries(loginFormData)
+    let options = {
+        'method': 'POST',
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        'body': JSON.stringify(loginObjectData)
+    }
+    fetch(loginEndpoint, options)
+    .then(response => {
+        return response.json()
+    })
+}
