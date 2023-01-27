f97779dd00289c721ced9415a48b5404dd6a0d26
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-01-27 08:06:47 +0200

    api

--- js_client/client.js
+++ js_client/client.js
@@ -1,8 +1,12 @@
 let contentContainer = document.getElementById('content-container')
 let loginForm = document.getElementById('login-form')
+let searchForm = document.getElementById('search-form')
 let baseEndpoint = 'http://localhost:8000/api'
 if (loginForm){
     loginForm.addEventListener('submit', handleLogin)
+}
+if (searchForm){
+    searchForm.addEventListener('submit', handleSearch)
 }
 
 function handleLogin(event){
@@ -23,6 +27,52 @@ function handleLogin(event){
     })
     .then(authData => {
         handleAuthData(authData, getProductList)
+    })
+    .catch(err=> {
+        console.log('err', err)
+    })
+
+}
+
+function handleSearch(event){
+    event.preventDefault()
+
+    let formData = new FormData(searchForm)
+    let data = Object.fromEntries(formData)
+    let searchParams = new URLSearchParams(data)
+    let endpoint = `${baseEndpoint}/search/?${searchParams}`
+    let headers = {
+        'Content-Type': 'application/json',
+    }
+    let authToken = localStorage.getItem('access')
+    if (authToken) {
+        headers['Authorization'] = `Bearer ${authToken}`
+    }
+    let options = {
+        'method': 'GET',
+        'headers': headers
+    }
+    fetch(endpoint, options)
+    .then(response=>{
+        return response.json()
+    })
+    .then(data => {
+        const validData = isTokenNotValid(data)
+        if (validData && contentContainer){
+            contentContainer.innerHTML = ""
+            if (data && data.hits) {
+                let htmlStr  = ""
+                for (let result of data.hits) {
+                    htmlStr += "<li>"+ result.title + "</li>"
+                }
+                contentContainer.innerHTML = htmlStr
+                if (data.hits.length === 0) {
+                    contentContainer.innerHTML = "<p>No results found</p>"
+                }
+            } else {
+                contentContainer.innerHTML = "<p>No results found</p>"
+            }
+        }
     })
     .catch(err=> {
         console.log('err', err)
